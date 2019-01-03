aa470d4b3df214b640e3aad5a9bb31c9de120eb1
Maxine Gerhard <mgerhard@Rediker.com>
2019-01-03 09:00:39 -0500

    scroll changes

--- src/Components/InfiniteScroll.story.tsx
+++ src/Components/InfiniteScroll.story.tsx
@@ -1,23 +1,21 @@
 import * as React from "react";
 import { storiesOf } from "@storybook/react";
 import { setDisplayName, compose } from "recompose";
-import modules from "Modules";
+import { seedServiceList, initializeTestServices, fakeModelModule } from "redux-data-service";
+import { Grid, Typography, Paper, withStyles } from "@material-ui/core";
 
 import { InfiniteScroll } from "./InfiniteScroll";
-import { withStyle } from "Style";
-import { seedServiceList, initializeTestServices } from "redux-data-service";
-import { Grid, Typography, Spacer } from "./Layout";
-import { Paper, } from "@material-ui/core";
-import { PersonAvatar, PersonNameDisplay } from "Modules/Person";
-import { BirthDateDisplay } from "./Form";
 
 const InfiniteScrollStoryContainer = compose<{}, {}>(
   setDisplayName("InfiniteScrollStoryContainer"),
-  withStyle({
-    height: 500,
-    width: 500,
-    border: "solid 1px black",
-    backgroundColor: "white !important",
+  withStyles({
+    root: {
+      height: 500,
+      width: 500,
+      border: "solid 1px black",
+      backgroundColor: "white !important",
+      overflow: "auto",
+    },
   }),
 )(({ children, ...props }) => (
   <Paper {...props}>
@@ -31,39 +29,37 @@ interface IInfiniteScrollStoryContainerItemProps {
 
 const InfiniteScrollStoryContainerItem = compose<IInfiniteScrollStoryContainerItemProps, IInfiniteScrollStoryContainerItemProps>(
   setDisplayName("InfiniteScrollStoryContainerItem"),
-  withStyle({
-    padding: ({ theme }) => [theme.spacing.unit * 4, "!important"],
-    borderBottomColor: ({ theme }) => [theme.palette.slate.light, "!important"],
-    borderBottom: {
-      style: "solid",
-      width: 1
+  withStyles({
+    container: {
+      padding: "25px",
+      borderBottomColor: "grey",
+      borderBottom: "solid",
     },
   }),
 )(({ children, item, ...props }) => (
-  <Spacer {...props}>
     <Grid container spacing={16}>
-      <Grid item>
-        <PersonAvatar person={item} size={32} />
-      </Grid>
       <Grid container item xs spacing={8}>
         <Grid item xs={12}>
-          <PersonNameDisplay person={item} />
+        ID: {item.id}
         </Grid>
         <Grid item xs={12}>
           <Typography>
-            Born <BirthDateDisplay value={item.dateBirth} />
+          Title: {item.fullText}
           </Typography>
         </Grid>
       </Grid>
     </Grid>
-  </Spacer>
 ));
 
 storiesOf("InfiniteScroll", module)
   .addDecorator((story) => {
-    initializeTestServices(modules);
-    for ( let i = 1; i < 10; i++ ) {
-      seedServiceList<any>("person", 10, { organizationId: "1", pageNumber: i });
+    initializeTestServices(fakeModelModule);
+
+    seedServiceList<any>("fakeModel", 20, { fullText: "page 1" }, { queryParams: { page: 1 }, hasNext: true, nextPage: 2, hasPrevious: false });
+    seedServiceList<any>("fakeModel", 20, { fullText: "page 10" }, { queryParams: { page: 10 }, hasNext: false, hasPrevious: true, previousPage: 9 });
+
+    for (let i = 2; i < 10; i++) {
+      seedServiceList<any>("fakeModel", 20, { fullText: `page ${i}` }, { queryParams: { page: i }, hasNext: true, nextPage: i + 1, hasPrevious: true, previousPage: i - 1 });
     }
 
     return story();
@@ -72,8 +68,7 @@ storiesOf("InfiniteScroll", module)
     <InfiniteScroll
       containerComponent={InfiniteScrollStoryContainer}
       itemComponent={InfiniteScrollStoryContainerItem}
-      serviceName="person"
-      queryParams={{ pageNumber: 1 }}
+      modelName="fakeModel"
+      query={{ page: 1 }}
     />
-  ))
-  ;
+  ));
