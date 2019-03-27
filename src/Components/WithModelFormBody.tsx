1ba31e6af2a457a7d43bbf30e9f977acd0bfc3a9
Jonathan Ziemnicki <JZiemnicki@Rediker.com>
2019-03-27 15:18:28 -0400

    IO-2301 Adding missing prop onto IModelFormBodyProps

--- src/Components/WithModelFormBody.tsx
+++ src/Components/WithModelFormBody.tsx
@@ -7,11 +7,12 @@ import { IModel } from "redux-data-service";
 export interface IModelFormBodyProps<T extends IModel<any>> {
   model: T;
   readOnly?: boolean;
+  focusOnFirstInput?: boolean;
 }
 
 /**
  * An HOC to wrap the inputs of the body of a ModelForm.
- * The given `model` and `readOnly` props will be passed along as child context, where they will be used by the `ModelField`.
+ * The given `model`, `readOnly` and `focusOnFirstInupt props will be passed along as child context, where they will be used by the `ModelField`.
  */
 export function withModelFormBody<T extends IModel<any>, P = {}>() {
   return compose<IModelFormBodyProps<T>, IModelFormBodyProps<T> & P>(
@@ -19,9 +20,10 @@ export function withModelFormBody<T extends IModel<any>, P = {}>() {
     withContext(
       {
         model: PropTypes.object,
-        readOnly: PropTypes.bool
+        readOnly: PropTypes.bool,
+        focusOnFirstInput: PropTypes.bool
       },
-      ({ model, readOnly }) => ({ model, readOnly }),
+      ({ model, readOnly, focusOnFirstInput }) => ({ model, readOnly, focusOnFirstInput }),
     ),
   );
 }
